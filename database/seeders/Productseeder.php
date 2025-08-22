3776bf5f1b6e56af3c7731999c5f41d2050403e2
zakria <zakriabrohi@gmail.com>
2025-08-22 17:03:43 +0500

    add some prodacts

--- database/seeders/Productseeder.php
+++ database/seeders/Productseeder.php
@@ -15,37 +15,44 @@ class Productseeder extends Seeder
     public function run(): void
     {
     //    DB::table('products')->insert([
-    //          'name' => 'laptop', 
+    //          'name' => 'laptop',
     //          'price' => '12999',
     //          'Category' => 'Electronics',
     //          'Discription' => '7 GN ram',
     //          'Gallery' => 'C:\Users\Zakria\OneDrive\Pictures\laptop_017.jpg'
-    //    ]);    
+    //    ]);
 
 
-    // multiple Data 
+    // multiple Data
 
     DB::table('products')->insert([
-         [
-             'name' => 'Dell 2',
-             'price' => '29999',
-             'Category' => 'Electronics',
-             'Discription' => '7 GB ram',
-             'Gallery' => 'images/Dell2.jpg'
-         ],
-         [
-             'name' => 'HP',
-             'price' => '31000',
-             'Category' => 'Electronics',
-             'Discription' => '8 GB ram',
-             'Gallery' => 'images/HP.jpg'
-         ],         [
-             'name' => 'PC laptop',
-             'price' => '39000',
-             'Category' => 'Electronics',
-             'Discription' => '10 GB ram',
-             'Gallery' => 'images/PC_laptop.jpg'
-         ]
+        //  [
+        //      'name' => 'Dell 2',
+        //      'price' => '29999',
+        //      'Category' => 'Electronics',
+        //      'Discription' => '7 GB ram',
+        //      'Gallery' => 'images/Dell2.jpg'
+        //  ],
+        //  [
+        //      'name' => 'HP',
+        //      'price' => '31000',
+        //      'Category' => 'Electronics',
+        //      'Discription' => '8 GB ram',
+        //      'Gallery' => 'images/HP.jpg'
+        //  ],         [
+        //      'name' => 'PC laptop',
+        //      'price' => '39000',
+        //      'Category' => 'Electronics',
+        //      'Discription' => '10 GB ram',
+        //      'Gallery' => 'images/PC_laptop.jpg'
+        //  ],
+        //  [
+        //       'name' => 'iPhone',
+        //      'price' => '115999',
+        //      'Category' => 'Electronics',
+        //      'Discription' => 'this is iphone 13',
+        //      'Gallery' => 'images/iphone.jpg'
+        //  ]
         ]);
     }
 }
